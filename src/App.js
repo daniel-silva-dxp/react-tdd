3cbd640e719c9b8b0386f4037ff588ed45e944f9
Daniel Silva <dani3l.dxp@gmail.com>
2020-06-04 12:19:20 -0300

    feat: initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>DANIEL SILVA</div>
+    </div>
+  );
+}
+
+export default App;
