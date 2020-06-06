61ce2b5197e38293a056b085d4a31c0a03962c4a
Daniel Silva <dani3l.dxp@gmail.com>
2020-06-06 07:57:32 -0300

    feat: refatorando código

--- src/components/sum.js
+++ src/components/sum.js
@@ -1,3 +1,3 @@
-const sum = () => {};
+const sum = (x, y) => x + y;
 
 export default sum;
--- src/tests/sum.test.js
+++ src/tests/sum.test.js
@@ -2,7 +2,15 @@ import { expect } from 'chai';
 import sum from '../components/sum';
 
 describe('Test with Jest', () => {
-	it('Should sum be a function', () => {
+	it('should be a function - sum()', () => {
 		expect(sum).to.be.a('function');
-	});
+    });
+    
+    it('should return 3 - sum(1,2)', () => {
+        expect(sum(1,2)).to.be.equal(3);
+    })
+
+    it('should return 7 - sum(2,5)', () => {
+        expect(sum(2,5)).to.be.equal(7)
+    })
 });
