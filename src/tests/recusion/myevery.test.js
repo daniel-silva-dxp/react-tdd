93f19050f7c01cfec82deff1431af47d7a080d28
Daniel Silva <dani3l.dxp@gmail.com>
2020-06-08 16:06:10 -0300

    feat: implementando testes para o método recursivo de myevery

--- src/tests/recusion/myevery.test.js
+++ src/tests/recusion/myevery.test.js
@@ -1,28 +1,28 @@
 import { expect } from 'chai';
-import myevery from '../../components/recusion/myevery';
+import recursiveEvery from '../../components/recusion/myevery';
 
 describe('My Every test', () => {
 	test('should be a function', () => {
-		expect(myevery).to.be.a('function');
+		expect(recursiveEvery).to.be.a('function');
 	});
 
 	test('should return `true` when receiving `([], (item) => item)`', () => {
-		expect(myevery([], (item) => item)).to.be.equal(true);
+		expect(recursiveEvery([], (item) => item)).to.be.equal(true);
 	});
 
 	test('should return `false` when receiving `([0,2,3], (item) => item)`', () => {
-		expect(myevery([ 0, 2, 3 ], (item) => item)).to.be.equal(false);
+		expect(recursiveEvery([ 0, 2, 3 ], (item) => item)).to.be.equal(false);
 	});
 
 	test('should return `true` when receiving `([1,2,3], (item) => item > 0)`', () => {
-		expect(myevery([ 1, 2, 3 ], (item) => item > 0)).to.be.equal(true);
+		expect(recursiveEvery([ 1, 2, 3 ], (item) => item > 0)).to.be.equal(true);
 	});
 
 	test('should return `true` when receiving `([1,2,3], (item, index) => index === item - 1)`', () => {
-		expect(myevery([ 1, 2, 3 ], (item, index) => index === item - 1)).to.be.equal(true);
+		expect(recursiveEvery([ 1, 2, 3 ], (item, index) => index === item - 1)).to.be.equal(true);
 	});
 
 	test('should return `true` when receiving `([1,2,3], (item, index, arr) => arr.length === 3)`', () => {
-		expect(myevery([ 1, 2, 3 ], (item, index, arr) => arr.length === 3)).to.be.equal(true);
+		expect(recursiveEvery([ 1, 2, 3 ], (item, index, arr) => arr.length === 3)).to.be.equal(true);
 	});
 });
