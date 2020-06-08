19103a6818e4ce40f863d67903fa460cf7b1867f
Daniel Silva <dani3l.dxp@gmail.com>
2020-06-08 11:44:32 -0300

    feat: criando a função myfilter

---
+++ src/components/recusion/myfilter.js
@@ -0,0 +1,11 @@
+const myfilter = (arr, callback) => {
+	const newArr = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (callback(arr[i], i, arr)) {
+			newArr.push(arr[i]);
+		}
+	}
+	return newArr;
+};
+
+export default myfilter;
